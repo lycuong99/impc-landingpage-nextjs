78b5fa0379091829052ddfb468d82cf297c6f8c4
cuong <lycuong99@gmail.com>
2022-09-29 22:06:43 +0700

    up

--- components/ScrollToTopButton.js
+++ components/ScrollToTopButton.js
@@ -29,7 +29,22 @@ const ScrollToTopButton = ({ scrollTriggerRef }) => {
         } else window.scrollTo({ top: 0, behavior: "smooth" });
       }}
     >
-      <Image src="/assets/icons/arrow-up.svg" alt="arrow up icon" layout="fill" />
+      <div className="img-wrapper">
+        <svg
+          width="21"
+          height="39"
+          viewBox="0 0 21 39"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M10.793 38.5117L10.793 3.09708" stroke="white" strokeWidth="2" />
+          <path
+            d="M1.25586 9.53613L10.7937 1.48735L20.3315 9.53613"
+            stroke="white"
+            strokeWidth="2"
+          />
+        </svg>
+      </div>
     </button>
   );
 };
